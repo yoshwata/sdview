d66d085775aa7cd490d14483c527f16ebe8d1c16
yoshwata <yoshwata@yahoo-corp.jp>
2022-06-21 03:01:46 +0000

    test

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-// process.stdin.resume();
-// process.stdin.setEncoding('utf8');
 
-// var inputText = "";
-// process.stdin.on('data', function(chunk){
-//   inputText += chunk;
-// });
-
-// process.stdin.on('end', function(){
-//   inputText.split('\n').forEach(text => {
-// 		console.log(text);
-// 	});
-// });
 
 const axios = require('axios');
 const config = require('./config');
 axios.defaults.baseURL = config.baseURL;
-const exec = async () => {
+const exec = async ({ buildId }) => {
   try {
     const r = await axios.get(`/v4/auth/token?api_token=${config.usertoken}`)
-    console.log(r.data);
+    // console.log(r.data);
     const jwt = r.data.token;
 
     const b = await axios.get(
-      `/v4/builds/78209094`,
+      `/v4/builds/${buildId}`,
       {
         headers: {
           "accept": "application/json",
@@ -31,7 +19,22 @@ const exec = async () => {
       }
     );
 
-    console.log(b);
+    console.log(b.data.meta.build.pipelineId);
+    const pipelineId = b.data.meta.build.pipelineId;
+
+    const p = await axios.get(
+      `/v4/pipelines/${pipelineId}`,
+      {
+        headers: {
+          "accept": "application/json",
+          "Authorization": jwt
+        }
+      }
+    );
+
+    const csv = buildId + ',' + pipelineId + ',' + p.data.name;
+
+    console.log(csv);
 
 
   } catch (error) {
@@ -53,8 +56,22 @@ const exec = async () => {
   // });
 }
 
-exec();
-
-
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf8');
+
+var inputText = "";
+process.stdin.on('data', function (chunk) {
+  inputText += chunk;
+});
+
+process.stdin.on('end', function () {
+  inputText.split('\n').forEach(text => {
+    console.log("aaaaaaaaaaaaaa" + text)
+    if (text) {
+      exec({ buildId: text });
+    }
+  });
+});
