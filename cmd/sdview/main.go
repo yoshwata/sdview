548ac57722c6f397dcd9fcc395434e041e9712e2
yoshwata <yoshwata@yahoo-corp.jp>
2022-08-06 10:22:10 +0000

    version

--- cmd/sdview/main.go
+++ cmd/sdview/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 
@@ -13,6 +15,7 @@ var (
 	// Used for flags.
 	cfgFile     string
 	userLicense string
+	version     string
 
 	rootCmd = cmd.NewCmdSdView(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
 )
@@ -26,6 +29,16 @@ func main() {
 	pflag.CommandLine = flags
 
 	rootCmd := cmd.NewCmdSdView(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
+
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of sdview",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("version: %s\n", version)
+		},
+	}
+	rootCmd.AddCommand((versionCmd))
+
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
